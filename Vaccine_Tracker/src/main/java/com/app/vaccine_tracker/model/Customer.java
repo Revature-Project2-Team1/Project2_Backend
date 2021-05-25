a3c6be1d24a55ca94b47db7c734da10826cc42f0
Anthony-Tag <82408367+Anthony-Tag@users.noreply.github.com>
2021-05-24 20:56:13 -0400

    Update Customer.java

--- Vaccine_Tracker/src/main/java/com/app/vaccine_tracker/model/Customer.java
+++ Vaccine_Tracker/src/main/java/com/app/vaccine_tracker/model/Customer.java
@@ -1,8 +1,27 @@
+package com.app.vaccine_tracker.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
 
 @Entity
 @Table
-public class Customer{
-}
+@Data
+@NoArgsConstructor
+@ToString
+public class Customer extends User{
+
+    @Id
+    @Column(name = "customer_ssn")
+    private String customerSSN;
+
+    private String status;
+    @Column(name = "vaccine_type")
+    private String vaccineType;
+    private String dob;
+    private String lot;
+    @ManyToOne
+    @JoinColumn(name = "address_id")
+    private Address address;
