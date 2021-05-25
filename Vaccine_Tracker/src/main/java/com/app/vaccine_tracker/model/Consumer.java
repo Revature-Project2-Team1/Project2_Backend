841d5332a8aef5fd2d858cac64078f312556d6e5
Phuc Huynh <43251722+Phuc15@users.noreply.github.com>
2021-05-24 17:47:23 -0700

    customer updated

--- Vaccine_Tracker/src/main/java/com/app/vaccine_tracker/model/Consumer.java
+++
@@ -1,9 +0,0 @@
-package com.app.vaccine_tracker.model;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table
-public class Consumer extends User{
-}
--- Vaccine_Tracker/src/main/java/com/app/vaccine_tracker/model/User.java
+++ Vaccine_Tracker/src/main/java/com/app/vaccine_tracker/model/User.java
@@ -1,3 +1,33 @@
 package com.app.vaccine_tracker.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Email;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@ToString
 public class User {
+
+    @Id
+    @Email(message = "Invalid email")
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "professional_email")
+    private Professional professional;
+    @Column(name ="fullname", nullable = false)
+    private String fullName;
+    @Column(nullable = false)
+    private  String password;
+    @Column(unique = true, nullable = false)
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_ssn")
+    private Customer customer;
+
+
+
 }
