a3459be2933eacb9fd58db577d14538cd07617ba
Anthony-Tag <82408367+Anthony-Tag@users.noreply.github.com>
2021-05-24 23:11:05 -0400

    Update Provider.java

--- Vaccine_Tracker/src/main/java/com/app/vaccine_tracker/model/Provider.java
+++ Vaccine_Tracker/src/main/java/com/app/vaccine_tracker/model/Provider.java
@@ -15,24 +15,16 @@ import java.util.List;
 @ToString
 public class Provider extends User {
 
-<<<<<<< HEAD
     @Column(name = "provider_email")
     private String  providerEmail;
-
-=======
->>>>>>> 4b2b232b763971a4c01a8da1d4d6969bc30010fe
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "provider_id")
     private int providerId;
-<<<<<<< HEAD
-
     @OneToMany(mappedBy = "provider")
     private List<Clinic> clinicList = new ArrayList<>();
-
-=======
     @OneToMany
     @JoinColumn(name = "clinic_id")
     private Clinic clinic;
->>>>>>> 4b2b232b763971a4c01a8da1d4d6969bc30010fe
+
 }
