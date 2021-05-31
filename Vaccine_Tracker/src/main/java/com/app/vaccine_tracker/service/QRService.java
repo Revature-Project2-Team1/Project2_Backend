2eeed99fd439c914fdf61dfb23f3bcc94e49c298
aadilowrevature <abeil.adilow@revature.net>
2021-05-31 19:20:31 -0400

    qr code database verify added

--- Vaccine_Tracker/src/main/java/com/app/vaccine_tracker/service/QRService.java
+++ Vaccine_Tracker/src/main/java/com/app/vaccine_tracker/service/QRService.java
@@ -1,8 +1,16 @@
 package com.app.vaccine_tracker.service;
 
 import com.app.vaccine_tracker.model.Patient;
+import com.app.vaccine_tracker.model.QRrecord;
 
 public interface QRService {
 
 public Patient getPatient(String ssn);
+
+public QRrecord addQR(QRrecord qrRecord);
+public void deleteQR(String ssn);
+public QRrecord checkQR(String ssn);
+
+
+
 }
