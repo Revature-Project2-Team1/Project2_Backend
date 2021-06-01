dcb039270403e095b5a95c52e0324b50a3ec76e6
Phuc Huynh <43251722+Phuc15@users.noreply.github.com>
2021-05-31 20:42:01 -0700

    updated

--- Vaccine_Tracker/src/main/java/com/app/vaccine_tracker/service/PatientService.java
+++ Vaccine_Tracker/src/main/java/com/app/vaccine_tracker/service/PatientService.java
@@ -4,8 +4,8 @@ import com.app.vaccine_tracker.model.Patient;
 
 public interface PatientService {
 
-   public String PatientLoginWithUsername(String username, String password);
-   public String PatientLoginWithEmail(String email, String password);
+   public Patient PatientLoginWithUsername(String username, String password);
+   public Patient PatientLoginWithEmail(String email, String password);
    public boolean registerPatientAccount(String email, String username, String password, String customerSSN);
    public boolean validatePatientCredential(String ssn, String email);
    public void updatePassword(String email, String password);
